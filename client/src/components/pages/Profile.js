2c2922f910fea252213e24ceae2b1f736e6e5051
maiada <maiadacx@gmail.com>
2020-01-03 15:43:18 -0500

    finished static profile layout

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -6,7 +6,7 @@ import DefaultAvatar from './../../images/default-profile-avatar.jpg';
 const Profile = () => {
     return (
         
-        <div className="container mt-5">
+        <div className="container mt-5 mb-5">
              <section className="section-profile">
                 {/* Heading : Name & Title */}
                 <div className="row mb-5">
@@ -62,10 +62,96 @@ const Profile = () => {
                 </div>
                 </div>
              </section>
-             <section className="section-profile mt-5">
 
+             {/* BIO */}
+             <section className="section-profile mt-5">               
+                {/* Bio - Heading */}
+                <h2 className="heading-profile heading-profile--sub">
+                    Bio
+                </h2>            
+                {/* Bio - Text */}
+                <p>
+                    Here is where the user can write whatever they want about who they are and what they're doing on the platform.
+                </p>        
              </section>
-            
+
+             {/* PROJECTS */}
+             <section className="section-profile mt-5 mb-5">               
+                {/* Projects - Heading */}
+                <h2 className="heading-profile heading-profile--sub">
+                    Projects
+                </h2>            
+                {/* Projects - Text */}
+                <div className="project-box">
+                    <h3 className="heading-project--main ml-1 mt-2">
+                        Project Name
+                    </h3>
+
+                    <hr className="mb-5"/>
+
+
+                    <h3 className="heading-project--sub">
+                        Description
+                    </h3>
+                    <div className="project-tag--box">
+                    <span className="project-tag">Link</span>
+                    <span className="project-tag">Code</span>
+                    <span className="project-tag">Status</span>
+                    </div>
+                    <p>A brief description of the project and maybe what kinds of team members are being sought</p>
+
+                    <hr className="mb-5"/>
+
+
+                    
+                    <h3 className="heading-project--sub">
+                        Team
+                    </h3>
+                    <div className="project-tag--box">
+                    <span className="project-tag">Status: Open</span>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="card__team text-center">
+                            <img src={DefaultAvatar} alt="user avatar" className="avatar avatar--sm w-50" />
+                            <hr />
+                            <p><b>Name: </b> Team member name</p>
+                            <p><b>Role: </b> Role title</p>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="card__team text-center">
+                            <img src={DefaultAvatar} alt="user avatar" className="avatar avatar--sm w-50" />
+                            <hr />
+                            <p><b>Name: </b> Team member name</p>
+                            <p><b>Role: </b> Role title</p>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="card__team text-center">
+                            <img src={DefaultAvatar} alt="user avatar" className="avatar avatar--sm w-50" />
+                            <hr />
+                            <p><b>Name: </b> Team member name</p>
+                            <p><b>Role: </b> Role title</p>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="card__team text-center">
+                            <img src={DefaultAvatar} alt="user avatar" className="avatar avatar--sm w-50" />
+                            <hr />
+                            <p><b>Name: </b> Team member name</p>
+                            <p><b>Role: </b> Role title</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>     
+             </section>
+
         </div>
     )
 }
