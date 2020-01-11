7db6a21b5a33c5f6a71e5edc8d41b418a480693b
maiada <maiadacx@gmail.com>
2020-01-11 12:10:22 -0500

    added profile actions/types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,6 +4,7 @@ export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL ';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+//profile
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const GET_PROFILE = 'GET_PROFILE';
-
+export const PROFILE_ERROR = 'PROFILE_ERROR';
