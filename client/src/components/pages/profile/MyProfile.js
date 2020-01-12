d80615cfa43622385007fc8a1a9b9c995e47f844
maiada <maiadacx@gmail.com>
2020-01-12 16:09:14 -0500

    basic profile information renders based on current user. creates skills file and routes

--- client/src/components/pages/profile/MyProfile.js
+++ client/src/components/pages/profile/MyProfile.js
@@ -1,34 +1,99 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getCurrentProfile } from '../../../actions/profile';
 
-function MyProfile({ getCurrentProfile, auth, profile }) {
-//calls the getCurrentProfile function
+const MyProfile = ({ 
+    getCurrentProfile, 
+    auth: { user }, 
+    profile: { profile, loading} 
+}) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    const [profileData, setProfileData] = useState({
+    const profileData = {
+        status: loading || !profile.status ? "" : profile.status,
+        website: loading || !profile.website ? "" : profile.website,
+        location: loading || !profile.location ? "" : profile.location,
+        skills: loading || !profile.skills ? "" : profile.skills,
+    };
 
-    });
-
-
-    return (
-
+    return loading && profile === null ? ( <div>loading</div> ) : (
         <div className="container mt-5 mb-5">
             <section className="section-profile">
+
             {/* Heading : Name & Title */}
                 <div className="row mb-5">
                     <div className="col-sm-12 text-center">
-    <h1 className="heading-profile heading-profile--main mb-3"></h1>
-                        <h2 className="heading-profile heading-profile--sub"></h2>
+                        <h1 className="heading-profile heading-profile--main mb-3">{user.name}</h1>
+                        <h2 className="heading-profile heading-profile--sub">{profileData.status}</h2>
                     </div>
                 </div>
+
             <hr />
+
+            <div className="row mt-5">
+                    {/* Col 1: Avatar */}
+                    <div className="col-md-5 col-sm-12 text-center">
+                        <img src={user.avatar} alt="user avatar" className="avatar avatar--lg w-75 mb-5" />
+                    </div>
+                {/* Col 2: User overview */}
+                <div className="col-md-7 col-sm-12">
+                    <div className="buttons  mb-5">
+                        <Link to="/createmessage" className="button button--user-action mr-3">
+                            <i className="far fa-envelope"></i> Message
+                        </Link>
+                        <Link to="#" className="button button--user-action mr-3"> 
+                            <i className="far fa-star"></i> Favorite
+                        </Link>
+                        <Link to="/editprofile" className="button button--user-action">
+                            Edit Profile
+                        </Link>
+                    </div>
+                    
+                    <ul>
+                    {/* city, state */}
+                    <li>
+                        <span className="font-weight-bold">Location: </span> 
+                        {profileData.location}
+                    </li>
+                    {/* website */}
+                    <li>
+                        <span className="font-weight-bold">Website: </span> 
+                        <a href={profileData.website} target="_blank" rel="noopener noreferrer">
+                        {profileData.website}
+                        </a>
+                    </li>
+                    {/* skills */}
+                    <li>
+                        <span className="font-weight-bold">Skills: </span> 
+                        
+                        <li className="list-item--skill mr-1">UX</li>
+                        <li className="list-item--skill mr-1">UI</li>
+                        <li className="list-item--skill mr-1">React</li>
+                        <li className="list-item--skill mr-1">Node.js</li>
+                    </li>
+                    {/* social media */}
+                    <li className="mt-5"> 
+                        <li className="list-item--social">
+                            <i className="fab fa-twitter fa-2x mr-3"></i>
+                        </li>
+                        <li className="list-item--social">
+                            <i className="fab fa-instagram fa-2x mr-3"></i>
+                        </li> 
+                        <li className="list-item--social">
+                            <i className="fab fa-github-alt fa-2x mr-3"></i>
+                        </li> 
+                    </li>
+                </ul>
+                </div>
+                </div>
             </section>
         </div>
-    )
+        )
+ 
 }
 
 //brings in the state/actions and defined what type they are
