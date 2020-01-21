e06c8780751aa7f4bb8a82548722a48b41c71395
Maria <cascercerim@gmail.com>
2020-01-20 19:48:34 -0500

    logout working

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-function Navbar() {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        
+    <ul>
+        <div className="navbar__link">
+        <Link to="/favorites">Favorites <i className="fas fa-star"></i></Link>
+        <Link to="/mail">Mail <i className="fas fa-envelope"></i></Link>
+        <Link to="/profile">Profile <i className="fas fa-user"></i></Link>
+    </div>
+        
+        
+        <a onClick={logout} href='#!'>
+            <i className='fas fa-sign-out-alt'></i>{' '}
+            <span className='hide-sm'>Logout</span>
+            </a>
+    
+    </ul>
+
+
+    );
+
+    const guestLinks = (
+
+        <div className="navbar__link">
+        <Link to="/favorites">Favorites <i className="fas fa-star"></i></Link>
+        <Link to="/mail">Mail <i className="fas fa-envelope"></i></Link>
+        <Link to="/profile">Profile <i className="fas fa-user"></i></Link>
+    </div>
+                    
+                    
+    );
     return (
         <nav className="navbar">
             <div className="navbar__logobox">
@@ -9,6 +42,7 @@ function Navbar() {
                     Pairigy
             </div>
 
+            { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
             <div className="navbar__searchbox">
                 <form className="form-inline">
                     <i className="fas fa-search fa-2x" aria-hidden="true"></i>
@@ -17,15 +51,18 @@ function Navbar() {
                 </form>
             </div>
 
-            <div className="navbar__link">
-                <Link to="/favorites">Favorites <i className="fas fa-star"></i></Link>
-                <Link to="/mail">Mail <i className="fas fa-envelope"></i></Link>
-                <Link to="/profile">Profile <i className="fas fa-user"></i></Link>
-                <Link to="logout">Logout</Link>
-            </div>
+            
         </nav>
     )
 }
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
 
+const mapStateToProps = state => ({
+    auth: state.auth
+})
 
-export default Navbar;
+export default connect(mapStateToProps, { logout })(Navbar);
+
