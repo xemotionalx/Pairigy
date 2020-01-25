dc0dd94607734c9e4ad9f74e8ba8d2cdf750aee2
maiada <maiadacx@gmail.com>
2020-01-25 14:44:05 -0500

    search results display cards with user info

--- client/src/components/search/SearchResults.js
+++ client/src/components/search/SearchResults.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
 const SearchResults = ({search: {results}}) => {
   return (
@@ -9,12 +10,30 @@ const SearchResults = ({search: {results}}) => {
         Search Results
       </h1>
 
+      <div className="w-75">
+
       {results ? 
 
-        results.map(profile => <p>{profile.user.name}</p>)
+        results.map(profile => 
+          <div className="card w-80 p-2 mb-3">
+              <div className="row">
+                <div className="col-md-4 text-center">
+                  <img src={profile.user.avatar} className="w-100 avatar" />
+                </div>
+                <div className="col-md-8 px-3">
+                  <div className="card-block px-3">
+                    <Link to={`profile/{profile.user._id}`}>
+                    <h4 className="heading-size--s card-title">{profile.user.name}</h4>
+                    </Link>
+                    <p className="card-text">{profile.location} | {profile.status}</p>
+                  </div>
+                </div>
+            </div>    
+          </div>  
+          )
         : ""
         }
-
+      </div>
     </div>
   );
 };
